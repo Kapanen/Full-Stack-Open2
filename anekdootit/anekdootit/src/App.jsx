85094e33f08839dbe899f6ab1a2a273a5f31d037
ee kapanen <eetu.kapanen@tuni.fi>
2026-03-04 16:04:37 +0200

    1.14

--- anekdootit/anekdootit/src/App.jsx
+++ anekdootit/anekdootit/src/App.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -19,11 +13,36 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  const mostVotes = votes.indexOf(Math.max(...votes))
+  const maxVotes = Math.max(...votes)
+
+  const handlevote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const handlenextanecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
-      <p><Button handleClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="next anecdote" /></p>
+      <p>has {votes[selected] || 0} votes</p>
+      <p>
+        <button onClick={handlevote}>vote</button>
+        <button onClick={handlenextanecdote}>next anecdote</button>
+      </p>
+      <h2>Anecdote with most votes</h2>
+      {maxVotes === 0 ? (<p>no votes yet  </p>) : (
+        <>
+        <p> {anecdotes[mostVotes]}</p>
+      <p >has {votes[mostVotes]} votes</p>
+        </>
+      )}
     </div>
 )
 }
