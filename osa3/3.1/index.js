66dde7ef7af4e63f918fc3196c00014685c968b0
ee kapanen <eetu.kapanen@tuni.fi>
2026-04-05 21:08:33 +0300

    3.3 ja 3.4

--- osa3/3.1/index.js
+++ osa3/3.1/index.js
@@ -36,6 +36,22 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   const date = new Date()
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
